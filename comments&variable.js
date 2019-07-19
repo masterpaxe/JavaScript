6669fb8b2c34737496b3c3855e29c70b5ddfa9ed
OLUGBENRO <OLUGBENRO21516@cbn.gov.ng>
2019-07-19 21:26:21 +0100

    my first commit

---
+++ comments&variable.js
@@ -0,0 +1,19 @@
+//This is a sinfle comment
+
+
+/*
+This is a multiline comment
+*/
+let x = 10;
+
+let x,y 
+x = 10;
+y = 20;
+
+
+//Variables are case sensitive
+//Use meaningful name
+//Must not be a reserved word
+//Use camel casing
+//Should not start with numbers
+
---
+++ operators.js
@@ -0,0 +1,36 @@
+/****Arithmetic Operators***** */
+// let x = 4;
+// let y = 2;
+
+// let result = x + y;
+// console.log('Result is : '+result)
+
+let a = 10;
+let q = 2 * ++a
+console.log('value is '+q)
+
+let t = 5;
+t +=6;
+console.log('the value of t is :'+t)
+
+//Comparison Operators
+
+let x = 10;
+let y = 20;
+let result = x == y;
+
+console.log('result is: '+result)
+
+//== => equal to
+//=== => equal value and equal type
+//!=   => not equal to 
+// !== not equal value and type
+
+/*
+Ternary Operator: like if-then-else
+takes 3 arguments
+condition ? value if true : value if false
+*/
+
+let result2 = x > y ? x : y;
+console.log(result2)
